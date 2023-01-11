6011839b53078fb803dec69e39d43707aeac8ba0
Evgenii Zaikin <evgenii.zaikin2607@gmail.com>
2023-01-11 17:34:51 +0300

    Update mock data and refactoring Main page.

--- src/pages/MainPage.vue
+++ src/pages/MainPage.vue
@@ -6,13 +6,17 @@
       <v-btn icon="mdi-logout" @click="onExit"></v-btn>
       <template v-slot:extension>
         <v-tabs v-model="activeTab">
-          <v-tab v-for="tab in tabs" :value="tab">{{ tab }}</v-tab>
+          <v-tab v-for="(tab, index) in tabs" :key="tab + index" :value="tab">{{
+            tab
+          }}</v-tab>
         </v-tabs>
       </template>
     </v-toolbar>
     <v-window v-model="activeTab">
       <v-list lines="one">
-        <v-list-item v-for="item in items">{{ item }}</v-list-item>
+        <v-list-item v-for="(item, index) in items" :key="item + index">{{
+          item
+        }}</v-list-item>
       </v-list>
     </v-window>
   </v-card>
@@ -31,7 +35,7 @@ interface IMainPageData {
 export default {
   data(): IMainPageData {
     return {
-      activeTab: "Пункт меню 1",
+      activeTab: "",
     };
   },
   computed: {
@@ -39,27 +43,31 @@ export default {
       return store.state.auth.role;
     },
     tabs(): string[] {
-      return (
-        (data["role-tabs"] as Record<string, string[]>)[
-          store.state.auth.role
-        ] || []
+      const roleTabs: Record<string, string[]> = data["role-tabs"];
+      return data.tabs.filter((tab: string) =>
+        roleTabs[store.state.auth.role]?.includes(tab)
       );
     },
     items(): string[] {
-      const items: string[] =
-        (data.items as Record<string, string[]>)[this.activeTab] || [];
+      const tabItems: Record<string, string[]> = data["tab-items"];
+      const roleItems: Record<string, string[]> = data["role-items"];
+      const items: string[] = tabItems[this.activeTab];
       return items.filter((item: string) =>
-        (data["role-items"] as Record<string, string[]>)[
-          store.state.auth.role
-        ].includes(item)
+        roleItems[store.state.auth.role]?.includes(item)
       );
     },
   },
   methods: {
-    onExit(): void {
+    async onExit(): Promise<void> {
+      await router.push("/");
       store.commit("auth/setRole", null);
-      router.push("/");
     },
+  },
+  created() {
+    const roleTabs: Record<string, string[]> = data["role-tabs"];
+    if (roleTabs[store.state.auth.role]?.length) {
+      this.activeTab = roleTabs[store.state.auth.role][0];
+    }
   },
 };
 </script>
